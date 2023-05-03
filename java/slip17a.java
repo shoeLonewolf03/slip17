613e8d37823aeba3cce4fcce58f7a9fe2163c501
Durgesh Sahu <ppmd.shau@gmail.com>
2023-05-03 22:47:20 +0530

    Update slip17a.java

---
+++ java/slip17a.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class A2A5
+{
+    public static void main(String[] args) throws InterruptedException
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();    
+        for (int i = 0; i < input.length(); i++)
+        {
+            char c = input.charAt(i);
+            if (isVowel(c))
+            {
+                Thread.sleep(3000); //sleep for 3 seconds
+                System.out.println(c);
+            }
+        }
+        scanner.close();
+    }
+    public static boolean isVowel(char c)
+    {
+        return "AEIOUaeiou".indexOf(c) != -1;
+    }
+}
